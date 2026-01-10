44791de3befb96dbd303e6077e7f79cbda1b4cec
Virender Chauhan <bikuchauhan786@gmail.com>
2026-01-10 13:28:04 +0530

    adds link on homepage to internal-tools page

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -5,10 +5,14 @@ import grainImage from "@/assets/images/grain.jpg";
 import StarIcon from "@/assets/icons/star.svg";
 import HeroOrbit from "@/components/HeroOrbit";
 import SparkleIcon from "@/assets/icons/sparkle.svg";
+import buildIcon from "@/assets/icons/build.png";
 import Link from "next/link";
 export const HeroSection = () => {
   return (
-    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip" id="home">
+    <div
+      className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip"
+      id="home"
+    >
       <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
         <div
           className="absolute inset-0 -z-30 opacity-5"
@@ -23,36 +27,106 @@ export const HeroSection = () => {
   shouldOrbit?: boolean;
   shouldSpin?: boolean;
   spinDuration?: string; */}
-        <HeroOrbit size={430} rotation={-14} shouldOrbit orbitDuration="30s" shouldSpin spinDuration="20s">
+        <HeroOrbit
+          size={430}
+          rotation={-14}
+          shouldOrbit
+          orbitDuration="30s"
+          shouldSpin
+          spinDuration="20s"
+        >
           <SparkleIcon className="size-8 text-emerald-300/20" />
         </HeroOrbit>
-        <HeroOrbit size={440} rotation={79} shouldOrbit orbitDuration="33s" shouldSpin spinDuration="20s">
+        <HeroOrbit
+          size={440}
+          rotation={79}
+          shouldOrbit
+          orbitDuration="33s"
+          shouldSpin
+          spinDuration="20s"
+        >
           <SparkleIcon className="size-5 text-emerald-300/20" />
         </HeroOrbit>
-        <HeroOrbit size={520} rotation={-41} shouldOrbit orbitDuration="34s" shouldSpin spinDuration="20s">
+        <HeroOrbit
+          size={520}
+          rotation={-41}
+          shouldOrbit
+          orbitDuration="34s"
+          shouldSpin
+          spinDuration="20s"
+        >
           <div className="size-2 bg-emerald-300/20 rounded-full"></div>
         </HeroOrbit>
-        <HeroOrbit size={530} rotation={178} shouldOrbit orbitDuration="36s" shouldSpin spinDuration="20s">
+        <HeroOrbit
+          size={530}
+          rotation={178}
+          shouldOrbit
+          orbitDuration="36s"
+          shouldSpin
+          spinDuration="20s"
+        >
           <SparkleIcon className="size-10 text-emerald-300/20" />
         </HeroOrbit>
 
-        <HeroOrbit size={550} rotation={20} shouldOrbit orbitDuration="38s" shouldSpin spinDuration="20s">
+        <HeroOrbit
+          size={550}
+          rotation={20}
+          shouldOrbit
+          orbitDuration="38s"
+          shouldSpin
+          spinDuration="20s"
+        >
           <StarIcon className="size-12 text-emerald-300" />
         </HeroOrbit>
-        <HeroOrbit size={590} rotation={98} shouldOrbit orbitDuration="40s" shouldSpin spinDuration="20s">
+        <HeroOrbit
+          size={590}
+          rotation={98}
+          shouldOrbit
+          orbitDuration="40s"
+          shouldSpin
+          spinDuration="20s"
+        >
           <StarIcon className="size-8 text-emerald-300" />
         </HeroOrbit>
-        <HeroOrbit size={650} rotation={-5} shouldOrbit orbitDuration="42s" shouldSpin spinDuration="20s">
+        <HeroOrbit
+          size={650}
+          rotation={-5}
+          shouldOrbit
+          orbitDuration="42s"
+          shouldSpin
+          spinDuration="20s"
+        >
           <div className="size-2 bg-emerald-300/20 rounded-full"></div>
         </HeroOrbit>
-        <HeroOrbit size={710} rotation={144} shouldOrbit orbitDuration="44s" shouldSpin spinDuration="20s">
+        <HeroOrbit
+          size={710}
+          rotation={144}
+          shouldOrbit
+          orbitDuration="44s"
+          shouldSpin
+          spinDuration="20s"
+        >
           <SparkleIcon className="size-14 text-emerald-300/20" />
         </HeroOrbit>
-        <HeroOrbit size={720} rotation={85} shouldOrbit orbitDuration="46s" shouldSpin spinDuration="20s">
+        <HeroOrbit
+          size={720}
+          rotation={85}
+          shouldOrbit
+          orbitDuration="46s"
+          shouldSpin
+          spinDuration="20s"
+        >
           <div className="size-3 bg-emerald-300/20 rounded-full"></div>
         </HeroOrbit>
 
-        <HeroOrbit size={800} rotation={-72} shouldOrbit orbitDuration="48s" shouldSpin spinDuration="20s">
+        <HeroOrbit
+          size={800}
+          rotation={-72}
+          shouldOrbit
+          orbitDuration="48s"
+          shouldSpin
+          spinDuration="20s"
+        >
           <StarIcon className="size-28 text-emerald-300" />
         </HeroOrbit>
       </div>
@@ -74,24 +148,39 @@ export const HeroSection = () => {
         </div>
         <div className="max-w-lg mx-auto z-10">
           <h1 className="font-serif text-3xl text-center mt-8 tracking-wide md:text-5xl">
-          Full-Stack Gen-AI Developer | Turning Ideas into Reality ⚡
+            Full-Stack Gen-AI Developer | Turning Ideas into Reality ⚡
           </h1>
           <p
             className="mt-4 text-center text-white/60 md:text-lg ;
           "
           >
-            Hi, I’m Virender Chauhan, a passionate Full-Stack GenAI Developer specialized in building high-performance web applications. From intuitive frontends to scalable backends, I turn ideas into seamless digital experiences. Let’s build something amazing together
+            Hi, I’m Virender Chauhan, a passionate Full-Stack GenAI Developer
+            specialized in building high-performance web applications. From
+            intuitive frontends to scalable backends, I turn ideas into seamless
+            digital experiences. Let’s build something amazing together
           </p>
         </div>
-        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4 cursor-auto" >
-          <Link href="#projects" className="inline-flex gap-2 border border-white/15 px-6 h-12 rounded-xl items-center z-30">
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4 cursor-auto">
+          <Link
+            href="#projects"
+            className="inline-flex gap-2 border border-white/15 px-6 h-12 rounded-xl items-center z-30 hover:scale-105 hover:border-2 hover:border-white transition-all duration-300 ease-in-out"
+          >
             <span className="font-semibold">Explore my work</span>
             <ArrowDown className="size-4" />
           </Link>
-          
-          <Link href="#contact" className="inline-flex items-center border border-white gap-2 bg-white text-gray-900 h-12 px-6 rounded-xl z-30" >
-            <span className="text-3xl">👋</span>
-            <span className="font-semibold">Lets connect</span>
+
+          <Link
+            href="/internal-tools-for-small-teams"
+            className="inline-flex items-center border border-white/15 gap-2 bg-white/90 text-gray-900 h-12 px-6 rounded-xl z-30 hover:scale-105 hover:bg-white transition-all duration-300 ease-in-out"
+          >
+            <Image
+              src={buildIcon}
+              alt="Build Icon"
+              className="size-8"
+              height={100}
+              width={100}
+            />
+            <span className="font-semibold">Internal Tools</span>
           </Link>
         </div>
       </div>
